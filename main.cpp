de7bde02d9368424ae96a7f2bd078d51826ffe74
taurinho13 <146191931+taurinho13@users.noreply.github.com>
2023-09-28 14:12:58 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,1 +1,6 @@
-
+﻿Build started...
+1>------ Build started: Project: uzduotis, Configuration: Debug x64 ------
+1>uzduotis.cpp
+1>uzduotis.vcxproj -> C:\Users\taura\Desktop\New folder\uzduotis\x64\Debug\uzduotis.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build started at 14:05 and took 02.292 seconds ==========
