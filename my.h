68e43c1ea25590a87b998518e4b26854b2127a4b
taurinho13 <146191931+taurinho13@users.noreply.github.com>
2023-10-01 11:45:12 +0300

    Update my.h

--- my.h
+++ my.h
@@ -1,1 +1,25 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <algorithm> 
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+struct zmogus {
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    int egz;
+    float vid;
+    float med;
+};
+void processLine(const string& line, vector<zmogus>& grupe);
+void inputStudentData(vector<zmogus>& grupe);
+void processFileData(vector<zmogus>& grupe);
+void printStudentData(const vector<zmogus>& grupe, int choice);
 
