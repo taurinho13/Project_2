c1766d312547bf9f80cd5010110c5e23e0c9c3ee
taurinho13 <146191931+taurinho13@users.noreply.github.com>
2023-10-15 12:39:50 +0300

    Update main.cpp
    included the function to generate student lists

--- main.cpp
+++ main.cpp
@@ -1,36 +1,48 @@
 #include "zmogus.h"
 
 int main() {
-    srand(time(0));
+    string choice;
+    cout << "Pasirinkite operacija (auto/file): ";
+    cin >> choice;
 
-    zmogus laikinas;
-    vector<zmogus> grupe;
-    int zmoniu_sk=0;
+    if (choice == "auto") {
+        generateStudentFilesAutomatically();
+    }
+    else if (choice == "file") {
+        srand(time(0));
 
-    string dataChoice;
-    cout << "Ar norite skaityti duomenis is failo (taip/ne): ";
-    cin >> dataChoice;
-    
-    if (dataChoice == "taip") {
-        processFileData(grupe);
+        zmogus laikinas;
+        vector<zmogus> grupe;
+        int zmoniu_sk = 0;
 
-        sort(grupe.begin(), grupe.end(), rikiavimas);
-        int choice;
-        cout << "Pasirinkite skaiciavimo buda (1 - Vidurkis, 2 - Mediana): ";
-        cin >> choice;
+        string dataChoice;
+        cout << "Ar norite skaityti duomenis is failo (taip/ne): ";
+        cin >> dataChoice;
 
-        ofstream outputFile("output.txt");
-        printStudentDataToFile(grupe, choice, outputFile);
-        outputFile.close(); 
+        if (dataChoice == "taip") {
+            processFileData(grupe);
+
+            sort(grupe.begin(), grupe.end(), rikiavimas);
+            int choice;
+            cout << "Pasirinkite skaiciavimo buda (1 - Vidurkis, 2 - Mediana): ";
+            cin >> choice;
+
+            ofstream outputFile("output.txt");
+            printStudentDataToFile(grupe, choice, outputFile);
+            outputFile.close();
+        }
+        else {
+            inputStudentData(grupe);
+            int choice;
+            cout << "Pasirinkite skaiciavimo buda (1 - Vidurkis, 2 - Mediana): ";
+            cin >> choice;
+
+            sort(grupe.begin(), grupe.end(), rikiavimas);
+            printStudentData(grupe, choice);
+        }
     }
     else {
-        inputStudentData(grupe);
-        int choice;
-        cout << "Pasirinkite skaiciavimo buda (1 - Vidurkis, 2 - Mediana): ";
-        cin >> choice;
-
-        sort(grupe.begin(), grupe.end(), rikiavimas);
-        printStudentData(grupe, choice);
+        cout << "Neteisinga pasirinkimo operacija." << endl;
     }
     return 0;
 };
