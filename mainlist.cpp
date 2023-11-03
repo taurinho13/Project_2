10b68196c7328842f47016c7e89294876089d1b9
taurinho13 <146191931+taurinho13@users.noreply.github.com>
2023-11-03 15:23:40 +0200

    Create mainlist.cpp
    main funkcija pakoreguota, kad veiktu su list

---
+++ mainlist.cpp
@@ -0,0 +1,52 @@
+#include "zmogus.h"
+
+int main() {
+    string choice;
+    cout << "Pasirinkite operacija (auto/file): ";
+    cin >> choice;
+
+    if (choice == "auto") {
+        generateStudentFilesAutomatically();
+    }
+    else if (choice == "file") {
+        srand(time(0));
+
+        zmogus laikinas;
+        list<zmogus> grupe;
+        int zmoniu_sk = 0;
+
+        string dataChoice;
+        cout << "Ar norite skaityti duomenis is failo (taip/ne): ";
+        cin >> dataChoice;
+
+        if (dataChoice == "taip") {
+            processFileData(grupe);
+
+            grupe.sort([](const zmogus& a, const zmogus& b) {
+                return a.pavarde < b.pavarde;
+                });
+            int choice;
+            cout << "Pasirinkite skaiciavimo buda (1 - Vidurkis, 2 - Mediana): ";
+            cin >> choice;
+
+            ofstream outputFile("output.txt");
+            printStudentDataToFile(grupe, choice, outputFile);
+            outputFile.close();
+        }
+        else {
+            inputStudentData(grupe);
+            int choice;
+            cout << "Pasirinkite skaiciavimo buda (1 - Vidurkis, 2 - Mediana): ";
+            cin >> choice;
+
+            grupe.sort([](const zmogus& a, const zmogus& b) {
+                return a.pavarde < b.pavarde;
+                });
+            printStudentData(grupe, choice);
+        }
+    }
+    else {
+        cout << "Neteisinga pasirinkimo operacija." << endl;
+    }
+    return 0;
+};
